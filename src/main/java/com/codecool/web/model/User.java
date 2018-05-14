13ae40cceeae59a988575a3e14227d1afe2acf37
hreskonorbert <norberthresko@gmail.com>
2018-05-14 10:45:41 +0200

    Created user class

---
+++ src/main/java/com/codecool/web/model/User.java
@@ -0,0 +1,33 @@
+package com.codecool.web.model;
+
+public class User extends AbstractModel {
+
+    private String name;
+    private String email;
+    private String password;
+    private String rank;
+
+    public User(int id,String name,String password,String email,String rank){
+        super(id);
+        this.name=name;
+        this.email=email;
+        this.password=password;
+        this.rank=rank;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+}
