acf625f5501652dd71b543111dcb86604acf2f7b
hreskonorbert <norberthresko@gmail.com>
2018-05-14 12:13:56 +0200

    Fixed issues with LoginService

--- src/main/java/com/codecool/web/service/LoginService.java
+++ src/main/java/com/codecool/web/service/LoginService.java
@@ -18,13 +18,15 @@ public class LoginService {
     public User getUserByName(String name, String password) throws SQLException, InvalidUserException {
         User user = userDao.getUserByName(name);
         if (user != null) {
-            if (user.getPassword().equals(password)) {
+            if (user.getPassword().equals(password) &&user.getName().equals(name)) {
                 return user;
             } else {
                 throw new InvalidUserException();
             }
+        }else{
+            throw new InvalidUserException();
         }
-        return null;
+
     }
 
 
