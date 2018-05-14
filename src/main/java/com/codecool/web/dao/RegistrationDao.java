2dce89aa52cf9c620eb272c20da3008e8dbf1e55
Domokos Tibor <Domokos.Tibor.82@gmail.com>
2018-05-14 12:30:31 +0200

    first & last name

--- src/main/java/com/codecool/web/dao/RegistrationDao.java
+++ src/main/java/com/codecool/web/dao/RegistrationDao.java
@@ -15,11 +15,13 @@ public class RegistrationDao extends AbstractDao {
 
         boolean autoCommit = connection.getAutoCommit();
         connection.setAutoCommit(false);
-        String sql = "INSERT INTO users (name, password, email, rank) VALUES (?, ?, ?, 'User')";
+        String sql = "INSERT INTO users (name, first_name, last_name, password, email, rank) VALUES (?, ?, ?, ?, ?, 'User')";
         try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);) {
             statement.setString(1, name);
-            statement.setString(2, pass);
-            statement.setString(3, email);
+            statement.setString(2, fname);
+            statement.setString(3, lname);
+            statement.setString(4, pass);
+            statement.setString(5, email);
             executeInsert(statement);
             int id = fetchGeneratedId(statement);
             connection.commit();
