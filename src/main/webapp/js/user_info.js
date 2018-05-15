bc2821e749d111a0e507a7327f9748a3d86a4e59
Domokos Tibor <Domokos.Tibor.82@gmail.com>
2018-05-15 09:48:29 +0200

    js basics

--- src/main/webapp/js/user_info.js
+++ src/main/webapp/js/user_info.js
@@ -1,5 +1,8 @@
 function userInfo() {
-    document.getElementById("userName").innerHTML = "Name: XY";
+    const userName = document.getElementById("actualUser");
+    const user = userName.value;
+    usern = user.name;
+    document.getElementById("userName").innerHTML = usern;
     document.getElementById("userFName").innerHTML = "First name: XXX";
     document.getElementById("userLName").innerHTML = "Last name: YYY";
     document.getElementById("userEmail").innerHTML = "Email: XXX@YYY.hu";
