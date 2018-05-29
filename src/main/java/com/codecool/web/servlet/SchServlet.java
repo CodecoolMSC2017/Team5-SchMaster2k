9c3a6804a4e1904d27bfac871d1b9822f4d7058b
ambrusBence1 <o.g.bence@gmail.com>
2018-05-29 14:28:48 +0200

    schServlet logging added

--- src/main/java/com/codecool/web/servlet/SchServlet.java
+++ src/main/java/com/codecool/web/servlet/SchServlet.java
@@ -4,7 +4,9 @@ import com.codecool.web.dao.DatabaseSchDao;
 import com.codecool.web.dto.ScheduleInformationDto;
 import com.codecool.web.model.Day;
 import com.codecool.web.model.Schedule;
+import com.codecool.web.model.User;
 import com.codecool.web.service.SchService;
+import org.apache.log4j.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -17,6 +19,8 @@ import java.util.List;
 
 @WebServlet("/schServlet")
 public class SchServlet extends AbstractServlet {
+    private static final Logger logger = Logger.getLogger(SchServlet.class);
+    private User user;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -44,19 +48,25 @@ public class SchServlet extends AbstractServlet {
             int id = Integer.parseInt(req.getParameter("userId"));
             DatabaseSchDao db = new DatabaseSchDao(c);
             SchService service = new SchService(db);
+            user = (User)req.getSession().getAttribute("user");
 
             int userId = Integer.parseInt(req.getParameter("userId"));
             if (req.getParameter("schTitle") != null) {
                 String schTitle = req.getParameter("schTitle");
                 String schContent = req.getParameter("schContent");
                 db.addSchedule(schTitle, schContent, userId);
+                logger.info(user.getName() + ": Add sch, sch name: " +schTitle);
+            } else {
+                logger.warn(user.getName() + ": Missing sch title");
             }
+
             List<Schedule> schedules = db.getAllSchByUserId(userId);
             resp.setContentType("application/json");
             sendMessage(resp, HttpServletResponse.SC_OK, schedules);
 
         } catch (SQLException e) {
             handleSqlError(resp, e);
+            logger.error(user.getName() + ": sch adding error");
         }
     }
 }
