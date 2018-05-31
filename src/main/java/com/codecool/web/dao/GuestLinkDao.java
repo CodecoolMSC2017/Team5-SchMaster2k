3cbf83b503aefcd094400e44125042333eb0775d
ambrusBence1 <o.g.bence@gmail.com>
2018-05-31 13:32:03 +0200

    skeleton for insert and del guest link datas to table

---
+++ src/main/java/com/codecool/web/dao/GuestLinkDao.java
@@ -0,0 +1,26 @@
+package com.codecool.web.dao;
+
+import java.sql.Connection;
+
+public class GuestLinkDao extends AbstractDao{
+
+    public GuestLinkDao(Connection connection) {
+        super(connection);
+    }
+
+    public void fnForInsertRENAME(){
+
+    }
+
+    public void fnForInsertRENAME22(){
+
+    }
+
+    public void fnForDeleteRENAME(){
+
+    }
+
+    public void fnForDeleteRENAME22(){
+
+    }
+}
---
+++ src/main/java/com/codecool/web/service/GuestLinksService.java
@@ -0,0 +1,20 @@
+package com.codecool.web.service;
+
+import com.codecool.web.dao.GuestLinkDao;
+
+public class GuestLinksService {
+
+    private GuestLinkDao db;
+
+    public GuestLinksService(GuestLinkDao db) {
+        this.db = db;
+    }
+    
+    public void insertGuestLink(int userId, int schId){
+
+    }
+
+    public void deleteGuestLink(int userId, int schId){
+
+    }
+}
